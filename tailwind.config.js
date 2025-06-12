73fc27f11d9c9315ada096cd69dcfef9425e08d1
Medusa <devoye.maysson@gmail.com>
2025-06-12 15:12:33 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate'
+
 export default {
   darkMode: ['class'],
   content: [
@@ -80,5 +82,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animate],
 };
